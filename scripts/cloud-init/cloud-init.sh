f1a0aa51f307251ea7b381f1aafa2297879c4c8f
Aaron Wislang <aaron.wislang@microsoft.com>
2021-02-24 22:41:11 -0500

    tee -a, no sudu on 09-vm.sh

--- scripts/cloud-init/cloud-init.sh
+++ scripts/cloud-init/cloud-init.sh
@@ -5,6 +5,5 @@ export DEBIAN_FRONTEND=noninteractive
 
 git clone https://github.com/asw101/dev
 cd dev/
-sudo bash scripts/00-setup-cloud-init.sh | tee output.txt
-sudo bash scripts/09-vm.sh | tee output.txt
-
+sudo bash scripts/00-setup-cloud-init.sh | tee -a output.txt
+bash scripts/09-vm.sh | tee -a output.txt
