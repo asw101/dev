518fcf8e14b5a33c4ce063c6f49fcf06b98f35bd
Aaron Wislang <aaron.wislang@microsoft.com>
2021-03-23 01:27:27 +0000

    move checkout

--- 10-cloud-init/cloud-init.sh
+++ 10-cloud-init/cloud-init.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 git clone https://github.com/asw101/dev
-git checkout dev
 cd dev/
+git checkout dev
 bash 00-setup.sh | tee -a ../output.txt
 echo "00-setup.sh complete!" | tee -a ../output.txt
