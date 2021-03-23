00ab3055a7d034c1aed33736b7e14f2be64c102c
Aaron Wislang <aaron.wislang@microsoft.com>
2021-03-23 01:22:15 +0000

    tweak cloud-init

--- 10-cloud-init/cloud-init.sh
+++ 10-cloud-init/cloud-init.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 git clone https://github.com/asw101/dev
+git checkout dev
 cd dev/
-bash 00-setup-cloud-init.sh | tee -a ../output.txt
-
-echo "00-setup-cloud-init.sh complete!" | tee -a ../output.txt
+bash 00-setup.sh | tee -a ../output.txt
+echo "00-setup.sh complete!" | tee -a ../output.txt
